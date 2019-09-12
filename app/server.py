af3868d4aed22f6a845e3af674e6bffd248dd499
Victoria Cabales <vcabales96@gmail.com>
2019-09-12 12:25:35 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -46,7 +46,7 @@ async def setup_learner(): # Load learner for predictions
         learn = load_learner('app/', export_file_name,test) # Replace path with path of file
         # learn.model = learn.model.module # must reference module since the learner was wrapped in nn.DataParallel
 #         preds = learn.get_preds(ds_type=DatasetType.Test)
-        logger.info("learner set up successfully")
+#         logger.info("learner set up successfully")
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
@@ -97,7 +97,7 @@ async def predict(request):
                 text = parser.from_file(my_file)['content'] # Pull down file from link
                 text = text.replace('\n',' ')
                 prediction = learn.predict(text)
-                logger.info("Prediction made")
+#                 logger.info("Prediction made")
                 print ("prediction made")
                 tensor_label = prediction[1].item()
                 if tensor_label == 0:
@@ -107,7 +107,7 @@ async def predict(request):
                     prob = prediction[2][1].item()
                     res = "Result: " + str(prediction[0]) + " - this school is not in danger of closing"
                 if str(prediction[0]) == 'last':
-                    logger.info([school, my_file, prob])
+#                     logger.info([school, my_file, prob])
                     csv_writer.writerow([school, my_file, prob])
                     csv_file.flush()
             csv_file.close()
